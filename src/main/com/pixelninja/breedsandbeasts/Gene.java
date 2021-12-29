3f742115eb31c94bd7e555beda74b7956fa5644e
PixelNinja5467 <81436718+PixelNinja5467@users.noreply.github.com>
2021-12-29 10:33:50 +0000

    Add files via upload

---
+++ src/main/com/pixelninja/breedsandbeasts/Gene.java
@@ -0,0 +1,15 @@
+package com.pixelninja.breedsandbeasts;
+
+import java.util.Iterator;
+
+public enum Gene {
+    Sheep,
+    Ender,
+    Creeper,
+    Flaming,
+    Goat,
+    Llama,
+    Ghastly,
+    Arthropod,
+    Undead;
+}
